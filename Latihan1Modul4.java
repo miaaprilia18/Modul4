ef30fe9dfb0a0b690623a55b2caefb4088637f86
miaaprilia18 <114122472+miaaprilia18@users.noreply.github.com>
2022-09-28 22:15:58 +0700

    Add files via upload

---
+++ Latihan1Modul4.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan1Modul4 {
+    public static void main( String[] args ){ 
+     
+      System.out.println("nummber =10\n"+"letter =a \n"+"result=true\n"+" str =hello  ");
+     }
+    
+}
